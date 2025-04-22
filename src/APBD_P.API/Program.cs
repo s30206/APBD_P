333bc3bc1c4376214363776bd2736d9f39725e13
s30206 <s30206@pjwstk.edu.pl>
2025-04-22 19:56:06 +0200

    Moved all files into corresponding folders

---
+++ src/APBD_P.API/Program.cs
@@ -0,0 +1,132 @@
+using APBD_P1;
+using Microsoft.AspNetCore.Mvc;
+using WebApplication1;
+
+var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+string inputPath = "D:\\СSProjects\\APBD_P\\src\\APBD_P.Source\\TextFiles\\input.txt";
+
+IDeviceManager manager = DeviceManagerFactory.CreateDeviceManager(inputPath);
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.MapGet("/api/devices", () =>
+    Results.Ok(manager.ReturnAllDevices()));
+
+app.MapGet("/api/devices/{id}", (string id) =>
+{
+    var device = manager.GetById(id);
+    return device is null ? Results.NotFound() : Results.Ok(device);
+});
+
+app.MapPost("/api/devices", ([FromBody] DeviceTemplate device) =>
+{
+    try
+    {
+        Device d = null;
+        switch (device.Type)
+        {
+            case "SW":
+                d = new Smartwatch
+                {
+                    Id = device.Id,
+                    Name = device.Name,
+                    IsOn = device.IsOn,
+                    BatteryPercentage = device.BatteryPercentage,
+                };
+                break;
+            case "P":
+                d = new PersonalComputer
+                {
+                    Id = device.Id,
+                    Name = device.Name,
+                    IsOn = device.IsOn,
+                    OperatingSystem = device.OperatingSystem
+                };
+                break;
+            case "ED":
+                d = new EmbeddedDevice
+                {
+                    Id = device.Id,
+                    Name = device.Name,
+                    IsOn = device.IsOn,
+                    IpAddress = device.IpAddress,
+                    NetworkName = device.NetworkName
+                };
+                break;
+        }
+
+        if (d is null)
+        {
+            return Results.BadRequest();
+        }
+        
+        manager.AddDevice(device);
+        return Results.Created($"/api/devices/{device.Id}", d);
+    }
+    catch (Exception e)
+    {
+        return Results.BadRequest(e.Message);
+    }
+});
+
+app.MapPut("/api/devices/{id}", (string id, [FromBody] DeviceTemplate newDevice) =>
+{
+    Device d = null;
+    switch (newDevice.Type)
+    {
+        case "SW":
+            d = new Smartwatch
+            {
+                Id = newDevice.Id,
+                Name = newDevice.Name,
+                IsOn = newDevice.IsOn,
+                BatteryPercentage = newDevice.BatteryPercentage,
+            };
+            break;
+        case "P":
+            d = new PersonalComputer
+            {
+                Id = newDevice.Id,
+                Name = newDevice.Name,
+                IsOn = newDevice.IsOn,
+                OperatingSystem = newDevice.OperatingSystem
+            };
+            break;
+        case "ED":
+            d = new EmbeddedDevice
+            {
+                Id = newDevice.Id,
+                Name = newDevice.Name,
+                IsOn = newDevice.IsOn,
+                IpAddress = newDevice.IpAddress,
+                NetworkName = newDevice.NetworkName
+            };
+            break;
+    }
+
+    if (d is null)
+    {
+        return Results.BadRequest();
+    }
+    
+    return manager.EditDataById(id, d)
+        ? Results.Ok(d)
+        : Results.NotFound();
+});
+
+app.MapDelete("/api/devices/{id}", (string id) => manager.RemoveDeviceById(id)
+    ? Results.Ok()
+    : Results.NotFound());
+
+app.Run();
---
+++ src/APBD_P.Console/Program.cs
@@ -0,0 +1,31 @@
+﻿using APBD_P1;
+
+// Relative paths work in API but not here. No idea why :D
+string inputPath = "D:\\СSProjects\\APBD_P\\src\\APBD_P.Source\\TextFiles\\input.txt";
+string outputPath = "D:\\СSProjects\\APBD_P\\src\\APBD_P.Source\\TextFiles\\output.txt";
+
+IDeviceManager manager = DeviceManagerFactory.CreateDeviceManager(inputPath);
+Console.WriteLine(manager.ReturnAllDevices()); // 5 devices because 2 lines in the input file have wrong data
+
+Console.WriteLine("Smartwatch");
+manager.TurnOffDevice(manager.Devices[0]);
+manager.TurnOnDevice(manager.Devices[0]); // Receive warning about low battery
+
+Console.WriteLine("Embedded device");
+manager.TurnOnDevice(manager.Devices[3]); // No errors
+manager.TurnOnDevice(manager.Devices[4]); // Errors (No MD Ltd.)
+
+Console.WriteLine("Personal computer");
+manager.TurnOnDevice(manager.Devices[1]); // No errors
+manager.TurnOnDevice(manager.Devices[2]); // Errors (no OS)
+
+Console.WriteLine($"Before adding a device: {manager.Devices.Count}");
+manager.AddDevice(new Smartwatch());
+Console.WriteLine($"After adding a device: {manager.Devices.Count}");
+manager.RemoveDevice(manager.Devices[manager.Devices.Count - 1]);
+Console.WriteLine($"After removing a device: {manager.Devices.Count}");
+
+Console.WriteLine("Deleting all data from the output file");
+File.WriteAllText(outputPath, string.Empty);
+manager.SaveData(outputPath);
+Console.WriteLine($"Saved data:\n{File.ReadAllText(outputPath)}");
