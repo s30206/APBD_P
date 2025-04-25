92f617dab3f6cedee81a1ea7e7c91485f22f822d
s30206 <s30206@pjwstk.edu.pl>
2025-04-25 10:50:15 +0200

    Fixed dependencies for APBD_P.Tests and fixed nuget packets for Windows

--- src/APBD_P.Source/obj/Debug/net8.0/APBD_P.Source.AssemblyInfo.cs
+++ src/APBD_P.Source/obj/Debug/net8.0/APBD_P.Source.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("APBD_P.Source")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+355edfa283edfa0cf40249b1569b659962a73f42")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1de2ac3aee8ba3741d5b4a02ae71114bfd033abf")]
 [assembly: System.Reflection.AssemblyProductAttribute("APBD_P.Source")]
 [assembly: System.Reflection.AssemblyTitleAttribute("APBD_P.Source")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
